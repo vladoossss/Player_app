c2f1126e2f4bde3d3611a4b463cbeeb0e630f945
Vladislav <veklenkovlad@mail.ru>
2021-03-23 05:47:25 -0700

    Initial Commit

---
+++ Player/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Player
+//
+//  Created by user193665 on 3/23/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
